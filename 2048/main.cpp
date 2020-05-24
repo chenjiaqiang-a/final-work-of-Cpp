5e14a65b5d7c43e8eaa40abfbc2634cf7aaeee42
chenjiaqiang-a <2504919775@qq.com>
2020-05-25 00:09:54 +0800

    创建项目

---
+++ 2048/main.cpp
@@ -0,0 +1,8 @@
+#include <easy2d/easy2d.h>
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hellow World\n";
+	return 0;
+}
---
+++ XiaoXiaoLe/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <easy2d/easy2d.h>
+
+int main()
+{
+	std::cout << "Hollow world\n";
+	return 0;
+}
